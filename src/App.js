a7a0aa8ad69c2dcd416110aeefb99e2d62f0666c
Muhammadali <muhammad@Muhammads-MacBook-Pro.local>
2022-07-08 14:34:30 +0500

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
 
 function App() {
+  const [aa, setClick] = useState(false)
+  const [data, setData] = useState({
+    name: "Ali",
+    username: "Ali99"
+  })
+  const makeObj = () => {
+    setData({ name: "Axmad", username: "Ortiqov" })
+  }
+  console.log(data);
+
+  // const [dataArr, setDataArr] = useState([
+  //   {
+  //     id: 1,
+  //     name: "Ali",
+  //     username: "Ali99"
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Ali2",
+  //     username: "Ali992"
+  //   }
+  // ])
+  // console.log(dataArr);
+
+
+
+  // console.log("click >>>", aa);
+  // const changeBack = () => {
+  //   if (aa === false) {
+  //     setClick(true)
+  //   } else {
+  //     setClick(false)
+  //   }
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className={`App ${aa ? "dark" : ""}`}>
+      <h1 style={{ color: aa ? "white" : "black" }}>Salom</h1>
+      {/* <button onClick={changeBack}>change Back</button> */}
+      <br />
+      <br />
+      {/* <button onClick={() => { !aa ? setClick(true) : setClick(false) }}>change Back2</button> */}
+
+      <button onClick={makeObj}> MAke Obj</button>
+    </div >
   );
 }
 
